ce21caa5c540571b98abd6122b0194fc518ff4ca
Daria <daria.diachkova@mail.ru>
2024-01-27 16:36:38 +0300

    extend sample

--- src/multicast.ts
+++ src/multicast.ts
@@ -1,6 +1,18 @@
-import { from, Subject, multicast, Observable } from 'rxjs'
+import { Subject, multicast, Observable, takeUntil } from 'rxjs'
 
-const source: Observable<number> = from([1, 2, 3, 4, 5])
+const completeSubject = new Subject<void>()
+
+const source: Observable<number> = new Observable<number>((observer) => {
+  console.log('%cNew subscription', 'color: gold')
+  let i = 0
+  const interval = setInterval(() => {
+    observer.next(i++)
+  }, 1000)
+  return () => {
+    clearInterval(interval)
+  }
+}).pipe(takeUntil(completeSubject))
+
 const subject = new Subject()
 const _multicasted = source.pipe(multicast(subject))
 
@@ -14,5 +26,10 @@ _multicasted.subscribe({
 
 // This is, under the hood, `source.subscribe(subject)`:
 _multicasted.connect()
+
+setTimeout(() => {
+  completeSubject.next()
+  completeSubject.complete()
+}, 5000)
 
 export const multicasted = _multicasted
