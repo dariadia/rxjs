813f3ac86d6ffc0e87c7ef553bd33a51d826b909
Daria <daria.diachkova@mail.ru>
2024-01-28 11:37:22 +0300

    add async sample

---
+++ src/async-subject.ts
@@ -0,0 +1,28 @@
+import { AsyncSubject } from 'rxjs'
+
+// AsyncSubject emits ONLY the last value to all Observers ON completion.
+
+const asyncSubject = new AsyncSubject<number>()
+asyncSubject.subscribe({
+  next: (value) => console.log('before:', value),
+  error: console.error,
+  complete: () => console.log('complete before')
+})
+
+// Emit some values.
+asyncSubject.next(1)
+asyncSubject.next(2)
+asyncSubject.next(3)
+
+// Subscribe late to subject.
+asyncSubject.subscribe({
+  next: (value) => console.log('after:', value),
+  error: console.error,
+  complete: () => console.log('complete after')
+})
+
+// Complete the observable stream.
+// If we do not complete, the AsyncSubject will never emit a next notification.
+asyncSubject.complete()
+
+export default asyncSubject
